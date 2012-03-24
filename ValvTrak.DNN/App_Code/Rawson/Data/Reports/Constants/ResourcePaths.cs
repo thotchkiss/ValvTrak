758a762daf61476e7fbb03015c8057625fb92f3f
Tim Hotchkiss <tim@thotchkiss.org>
2012-03-24 12:49:42 -0500

    Updated reporting resource strings again.

--- ValvTrak.DNN/App_Code/Rawson/Data/Reports/Constants/ResourcePaths.cs
+++ ValvTrak.DNN/App_Code/Rawson/Data/Reports/Constants/ResourcePaths.cs
@@ -11,10 +11,10 @@ namespace Rawson.Reports.Constants
     public class ResourcePaths
     {
         public static string ReportServerPath = HttpContext.Current.Request.Url.Host == "localhost" ? "http://valvtrak.rawsonenergyservices.com/reportserver/reportservice.asmx" : "http://localhost/reportserver/reportservice.asmx";
-        public static string DownloadHelperPath = HttpContext.Current.Request.Url.Host == "localhost" ? "http://localhost/srd/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}" : "http://valvtrak.rawsonenergyservices.com/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
+        public static string DownloadHelperPath = HttpContext.Current.Request.Url.Host == "localhost" ? "http://localhost/beta/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}" : "http://valvtrak.rawsonenergyservices.com/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
         
         // Local testing only
-        //public const string DownloadHelperPath = "http://localhost/srd/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
+        //public const string DownloadHelperPath = "http://localhost/beta/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
 
     }
 }
