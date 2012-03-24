8adf6d2d2f4dba689e04543f7cf7d18b489a6ff3
Tim Hotchkiss <tim@thotchkiss.org>
2012-03-24 16:09:07 -0500

    Updated reporting resource strings again.

--- ValvTrak.DNN/App_Code/Rawson/Data/Reports/Constants/ResourcePaths.cs
+++ ValvTrak.DNN/App_Code/Rawson/Data/Reports/Constants/ResourcePaths.cs
@@ -10,11 +10,7 @@ namespace Rawson.Reports.Constants
     /// </summary>
     public class ResourcePaths
     {
-        public static string ReportServerPath = HttpContext.Current.Request.Url.Host == "localhost" ? "http://valvtrak.rawsonenergyservices.com/reportserver/reportservice.asmx" : "http://localhost/reportserver/reportservice.asmx";
+        public static string ReportServerPath = "http://valvtrak.rawsonenergyservices.com/reportserver/reportservice.asmx";
         public static string DownloadHelperPath = HttpContext.Current.Request.Url.Host == "localhost" ? "http://localhost/beta/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}" : "http://valvtrak.rawsonenergyservices.com/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
-        
-        // Local testing only
-        //public const string DownloadHelperPath = "http://localhost/beta/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
-
     }
 }
