22c018cd77d07e12f19829e0553fc378e01efb00
Tim Hotchkiss <tim@thotchkiss.org>
2012-03-26 21:41:33 -0500

    Updated RES logo in current reports.
    Added Silverlight mapping application.

---
+++ ValvTrak.Silverlight.Maps/ValvTrak.Silverlight.Maps/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Layout.Core;
+using DevExpress.Xpf.Docking;
+
+
+namespace ValvTrak.Silverlight.Maps
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ThemeManager.ApplicationThemeName = "Office2007Blue";
+
+        }
+    }
+
+
+}
