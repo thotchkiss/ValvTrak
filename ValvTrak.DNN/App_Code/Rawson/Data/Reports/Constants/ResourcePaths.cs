74f3b12293e1d3bc710bcdbf8abd71d8634017f7
Tim Hotchkiss <tim@thotchkiss.org>
2012-04-12 20:27:52 -0500

    Modified reporting resources class with extension method.
    Removed tolerance validation from valve test form.

--- ValvTrak.DNN/App_Code/Rawson/Data/Reports/Constants/ResourcePaths.cs
+++ ValvTrak.DNN/App_Code/Rawson/Data/Reports/Constants/ResourcePaths.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 
 namespace Rawson.Reports.Constants
 {
@@ -10,7 +11,23 @@ namespace Rawson.Reports.Constants
     /// </summary>
     public class ResourcePaths
     {
-        public static string ReportServerPath = "http://valvtrak.rawsonenergyservices.com/reportserver/reportservice.asmx";
-        public static string DownloadHelperPath = HttpContext.Current.Request.Url.Host == "localhost" ? "http://localhost/beta/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}" : "http://valvtrak.rawsonenergyservices.com/desktopmodules/rawson.reports/downloadhelper.aspx?ReportPath={0}&cacheKey={1}";
+        public static string ReportServerPath { get { return "http://valvtrak.rawsonenergyservices.com/reportserver/reportservice.asmx"; } }
+        public static string DownloadHelperPath { get { return String.Format("http://{0}/desktopmodules/rawson.reports/downloadhelper.aspx{1}", HttpContext.Current.Request.Url.GetDownloadHelperPath(), "?ReportPath={0}&cacheKey={1}"); } }
+    }
+
+    static class Extensions
+    {
+        public static string GetDownloadHelperPath(this Uri uri)
+        {
+            StringBuilder path = new StringBuilder();
+
+            path.Append(uri.Host);
+            path.Append(":");
+            path.Append(uri.Port);
+            path.Append("/");
+            path.Append(uri.Segments.ToList().Contains("app") ? "app" : "beta");
+
+            return path.ToString();
+        }
     }
 }
